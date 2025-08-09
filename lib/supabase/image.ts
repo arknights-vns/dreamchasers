db22c983be197a137da60d2883972e34db81e23c
Tien Dat Pham <dat20036@gmail.com>
2025-08-09 23:37:08 +0700

    feat: not yet.

--- lib/supabase/image.ts
+++ lib/supabase/image.ts
@@ -9,7 +9,7 @@ type ImageProps = {
 // Docs: https://supabase.com/docs/guides/storage/image-transformations#nextjs-loader
 export default function supabaseLoader({ src, width, quality }: ImageProps) {
     const supabaseHost = process.env.NEXT_PUBLIC_SUPABASE_CDN_URL ?? process.env.NEXT_PUBLIC_SUPABASE_URL;
-    const directory = supabaseHost.endsWith("supabase.co") ? "object" : "render/image";
+    const directory = supabaseHost!.endsWith("supabase.co") ? "object" : "render/image";
     const url = new URL(`${supabaseHost}/storage/v1/${directory}/public${src}`);
     url.searchParams.set("width", width.toString());
     url.searchParams.set("quality", (quality || 75).toString());
